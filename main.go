ef33e36bf16a1dc788c767dc1a89ad95825ff6f7
davislee168 <53437101+davislee168@users.noreply.github.com>
2020-04-29 10:43:09 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import {
-	“dlee/engine”
+	"engine"
 }
 
 func main() {
 	linkLimit := 25
-       url := “https://www.yahoo.com”
-       
-       engine.Run(url, linkLimit)
+	url := "https://www.yahoo.com"
+	
+	engine.Run(url, linkLimit)
 }
 
 
